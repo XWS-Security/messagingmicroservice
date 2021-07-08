708a0fc1896d96d04b1ea87f3bdd54bfb2b46d35
Milica Siriski <milica.siriski998@gmail.com>
2021-07-08 14:28:51 +0200

    User can accept/reject/delete conversations

--- src/main/kotlin/org/nistagram/messagingmicroservice/service/ConversationService.kt
+++ src/main/kotlin/org/nistagram/messagingmicroservice/service/ConversationService.kt
@@ -5,4 +5,7 @@ import org.nistagram.messagingmicroservice.controller.dto.ConversationDto
 interface ConversationService {
     fun getAll(): List<ConversationDto>
     fun start(participants: List<String>, token: String): Boolean
+    fun accept(id: Long)
+    fun reject(id: Long)
+    fun delete(id: Long)
 }
